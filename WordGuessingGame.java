f1d9811a35b3740b1188902b9a41e33ded0a2c9a
danilopacvic <202300224@estudantes.ips.pt>
2024-03-13 09:56:39 +0000

    Nível 2

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -1,12 +1,16 @@
+import java.util.Locale;
+
 public class WordGuessingGame {
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader reader;
 
     public WordGuessingGame() {
         this.hiddenWord = "abc";
         this.guessedWord = "___";
         this.numberOfTries = 0;
+        reader = new InputReader();
     }
 
     public String getHiddenWord(){
@@ -19,7 +23,36 @@ public class WordGuessingGame {
     public int getNumberOfTries(){
         return numberOfTries;
     }
-    public void ShowGuessedWord(){
+    public void showGuessedWord(){
         System.out.println("A palavra que se vai adivinhando é" +guessedWord);
     }
+
+    public void showWelcome(){
+        System.out.println("Welcome to the game!");
+    }
+
+    public void guess(char guess) {
+    for(int i =0; i< hiddenWord.length(); i++){
+     if(hiddenWord.toLowerCase().charAt(i) == guess) {
+         StringBuilder builder = new StringBuilder(getGuessedWord());
+         builder.setCharAt(i, guess);
+         guessedWord = builder.toString();
+            }
+        }
+    }
+    public void showResult(){
+        System.out.println("Foi realizado "+numberOfTries+" tentativas para adivinhar a palavra.");
+    }
+    public void play(){
+    showWelcome();
+    do{
+        char guessedLetter = reader.getChar("Digite a letra que deseja adivinhar:");
+        guess(guessedLetter);
+        showGuessedWord();
+        numberOfTries++;
+        }
+    while(!guessedWord.equals(hiddenWord));
+    }
 }
+
+
